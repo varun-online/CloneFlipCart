5b10359a8b754bf77980c25225c7d265fd0337df
Varun <varuncontacto@gmail.com>
2026-02-07 16:26:05 +0530

    Configure server port for Railway

---
+++ src/main/java/com/clone/CloneFlipCart/Controller/HealthController.java
@@ -0,0 +1,13 @@
+package com.clone.CloneFlipCart.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/")
+    public String health() {
+        return "CloneFlipCart is LIVE 🚀";
+    }
+}
